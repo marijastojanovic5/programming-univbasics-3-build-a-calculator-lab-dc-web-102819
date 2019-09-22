19b91181085a99b2473526a60df9412ecf14046b
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-09-22 22:02:37 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,15 +5,15 @@ end
 
 
 def subtraction(num1, num2)
-
+return num1-num2
 end
 
 def division(num1, num2)
-
+return num1/num2
 end
 
 def multiplication(num1, num2)
-
+return num1*num2
 end
 
 def modulo(num1, num2)
